c8e1e4ae9f8ae7c82122da484a24b619b3765b45
okeeman@gmail.com <okeeman@gmail.com@fe8eac2c-1429-41c5-6867-7c37e67d637c>
2012-12-15 14:32:10 +0000

    Azure Doctor v1.0 first iteration, partial upload

---
+++ trunk/mvc4app/AzureDoctor.Tests/Controllers/AzureDoctorControllerIntegrationTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AzureDoctor;
+using AzureDoctor.Controllers;
+//
+using AzureDoctor.Models;
+
+namespace AzureDoctor.Tests.Controllers
+{
+    [TestClass]
+    public class AzureDoctorControllerIntegrationTests
+    {
+        
+        [TestMethod]
+        public void TestMakeHealthRequest()
+        {
+            // Arrange.
+            CloudService cs = new CloudService();
+            cs.ServiceName = "housecondition";
+            cs.SubscriptionID = "7f5f45c1-da00-4435-9694-364ba894dbde";
+            cs.Thumbprint = "52D117A8589A69D81290B4FB2A11E5B26324FD88";
+            cs.UserID = 1;
+            cs.CloudServiceID = 1;
+
+            HealthModel hm = new HealthModel();
+                
+            // Act.
+            hm.MakeHealthRequest(cs);
+
+            // Assert.
+            Assert.AreEqual(hm.StatusList[0], "Created");
+            Assert.AreEqual(hm.StatusList[1], "Running");
+            Assert.AreEqual(hm.InstanceStatuses[0], "ReadyRole");
+            Assert.AreEqual(hm.InstanceStatuses[1], "ReadyRole");
+            Assert.AreEqual(hm.InstanceCount, 2);
+        }
+    }
+}
---
+++ trunk/mvc4app/AzureDoctor.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AzureDoctor;
+using AzureDoctor.Controllers;
+
+namespace AzureDoctor.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void TestHomeIndexRedirect()
+        {
+            // Arrange
+            var controller = new HomeController();
+
+            // Act
+            var result = controller.Index() as RedirectToRouteResult;
+
+            // Assert
+            // Redirects to AzureDoctorController, Index Action.
+            Assert.AreEqual("Index", result.RouteValues["action"]);
+        }
+
+        [TestMethod]
+        public void About()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.About() as ViewResult;
+
+            // Assert
+            Assert.AreEqual("About", result.ViewName);
+        }
+
+        [TestMethod]
+        public void Contact()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Contact() as ViewResult;
+
+            // Assert
+            Assert.AreEqual("Contact", result.ViewName);
+        }
+
+        [TestMethod]
+        public void SiteMap()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.SiteMap() as ViewResult;
+
+            // Assert
+            Assert.AreEqual("SiteMap", result.ViewName);
+        }
+    }
+}
