5ec5cfd5310189adc67b044d6bb4c10cf00b1e59
okeeman <okeeman@fe8eac2c-1429-41c5-6867-7c37e67d637c>
2013-03-08 21:16:33 +0000

    Updated web role unit tests

---
+++ trunk/webrole/AzureDoctor.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AzureDoctor;
+using AzureDoctor.Controllers;
+
+namespace AzureDoctor.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.AreEqual("Index", result.ViewName);
+        }
+
+        [TestMethod]
+        public void Contact()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Contact() as ViewResult;
+
+            // Assert
+            Assert.AreEqual("Contact", result.ViewName);
+        }
+    }
+}
