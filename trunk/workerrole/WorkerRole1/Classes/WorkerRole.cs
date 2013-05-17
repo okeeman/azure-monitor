fbde14eb3913695cc35b330bf67e288fc6b34ef6
okeeman <okeeman@fe8eac2c-1429-41c5-6867-7c37e67d637c>
2013-05-17 20:51:26 +0000

    Final iteration of Worker role

---
+++ trunk/workerrole/WorkerRole1/Classes/WorkerRole.cs
@@ -0,0 +1,145 @@
+// ----------------------------------------------------------------------------------
+// Owen Sweeney 2013
+// ----------------------------------------------------------------------------------
+// * The Worker Role makes health requests for all cloud services with a status of 
+// Active. A subscription may have many cloud services. Each cloud service may have 
+// many instances. If any instance is not in ReadyRole, i.e., ready to accept 
+// requests, then an error messaghe SMS is sent to the user's phone. 
+// * If the Run method throws an exception the exception message is sent to the Azure
+// Doctor administrator. 
+// * There are a number of Trace.WriteLines included. These prove useful
+// during local development & debugging using the Compute UI emulator.
+// ----------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Diagnostics;
+using Microsoft.WindowsAzure.ServiceRuntime;
+using Microsoft.WindowsAzure.StorageClient;
+// Added to bring IAzureDoctorDb and AzureDoctorDb into scope.
+using WorkerRole1.Models;
+// Added to bring XmlTextReader into scope.
+using System.Xml;
+
+namespace WorkerRole1.Classes
+{
+    public class WorkerRole : RoleEntryPoint
+    {
+        private IAzureDoctorDb _db;
+        PushNotifications pn = new PushNotifications();
+        const string ClickatellUsername = "okeeman";
+        const string ClickatellPassword = "ECMPIbIELWBCSA";
+
+        // Default constructor - hits real database.
+        public WorkerRole()
+        {
+            _db = new AzureDoctorDb();
+        }
+
+        // Fake database - used for unit tests.
+        public WorkerRole(IAzureDoctorDb db)
+        {
+            _db = db;
+        }
+
+        public Certificate Certificate
+        {
+            get
+            {
+                return _db.Query<Certificate>().First();
+            }
+        }
+
+        public override void Run()
+        {
+            Trace.WriteLine("Inside the Run method", "Information");
+
+            while (true)
+            {
+                try
+                {
+                    List<CloudService> cloudServiceCollection = new List<CloudService>();
+                    foreach (var service in _db.Query<CloudService>())
+                    {
+                        if (service.Status == "Active")
+                        {
+                            cloudServiceCollection.Add(service);
+                        }
+                    }
+
+                    foreach (var service in cloudServiceCollection)
+                    {
+                        // Find the subscription associated with the CloudService.
+                        Subscription sub = _db.Query<Subscription>().FirstOrDefault(s => s.SubscriptionRecordID == service.SubscriptionRecordID);
+
+                        Trace.WriteLine("sub: " + sub.AzureSubscriptionID, "Information");
+                        Trace.WriteLine("service: " + service.CloudServiceID, "Information");
+                        //Trace.WriteLine("cert: " + cert.Thumbprint, "Information");
+                        Trace.WriteLine("cert: " + this.Certificate.Thumbprint, "Information");
+                        Trace.WriteLine("service details: " + service.CloudServiceName, "Information");
+                        Trace.WriteLine("Before health request", "Information");
+                        // Send request to the Windows Azure API.
+                        XmlTextReader reader = ServiceMgmtAPICall.MakeHealthRequest(sub, service, this.Certificate);
+                        ParseResults pr = new ParseResults();
+                        RequestResult rr = pr.ParseXmlText(reader);
+                        rr.CloudServiceID = service.CloudServiceID;
+
+                        Trace.WriteLine("Before AllInstancesHealthy check", "Information");
+                        // Send SMS if the Cloud Service is unhealthy.
+                        if (rr.AllInstancesHealthy != true)
+                        {
+                            Trace.WriteLine("Not all instances are healthy, SMS push", "Information");
+                            Trace.WriteLine("Cloud service " + service.CloudServiceName + " has experienced an error", "Information");
+                            string message = String.Format("Your cloud service, {0}, has encountered an error", service.CloudServiceName);
+                            pn.PushSMS(sub.PhoneNumber, message, ClickatellUsername, ClickatellPassword);
+                        }
+                        else
+                        {
+                            Trace.WriteLine("Cloud service " + service.CloudServiceName + " is healthy", "Information");
+                        }
+
+                        Trace.WriteLine("Before save changes", "Information");
+                        _db.Add(rr);
+                        _db.SaveChanges();
+                        Trace.WriteLine("After save changes", "Information");
+                      
+                        //Get the RequestResultID which was generated in the database as a result of writing the RequestResult entity to it.
+                        //This is not known until the RequestResult entity is written to the database. It is a Foreign Key constraint in the InstanceStatu table.
+                        int maxID = _db.Query<RequestResult>().Where(r => r.CloudServiceID == service.CloudServiceID).Max(r => r.RequestID);
+                        Trace.WriteLine("maxID: " + maxID, "Information");
+
+                        foreach (var status in pr.InstanceStatuses)
+                        {
+                            Trace.WriteLine("Inside instance statuses foreach in Run method", "Information");
+                            InstanceStatu i = new InstanceStatu();
+                            i.InstanceStatus = status;
+                            i.RequestID = maxID;
+                            _db.Add(i);
+                            _db.SaveChanges();
+                        }
+                    }
+
+                    Thread.Sleep(30000);
+                }
+                catch (Exception e)
+                {
+                    Trace.WriteLine("The Run method has thrown an exception: " + e.Message + "\nInner exception: " + e.InnerException, "Information");
+                    string exceptionMessage = "The Run method has thrown an exception: " + e.Message + "\nInner exception: " + e.InnerException;
+                    // Sends SMS to the site admin rather than the users.
+                    pn.PushSMS("353872802526", exceptionMessage, ClickatellUsername, ClickatellPassword);
+                    Thread.Sleep(60000);
+                }
+            }
+        }
+
+        public override bool OnStart()
+        {
+            ServicePointManager.DefaultConnectionLimit = 12;
+            return base.OnStart();
+        }
+    }
+}
---
+++ trunk/workerrole/WorkerRoleUnitTests/UnitTests/IsolationUnit Tests.cs
@@ -0,0 +1,70 @@
+﻿// ----------------------------------------------------------------------------------
+// Owen Sweeney 2013
+// ----------------------------------------------------------------------------------
+// The Windows Azure Service Management API returns an XML file which has the
+// cloud service status in it. These tests use a fake results XML file to test the
+// Azure Doctor program logic. Some of the functionality from earlier iterations was 
+// refactored into separate classes in order to make it possible to test the program
+// logic without having to mahe HTTP requests or be connected to a database.
+// ----------------------------------------------------------------------------------
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+// Added to bring FakeAzureDoctorDb and TestData into scope.
+using WorkerRoleUnitTests.Fakes;
+// Added to bring FakeAzureDoctorDb and TestData into scope.
+using WorkerRole1.Classes;
+// Added to bring domain models into scope.
+using WorkerRole1.Models;
+// Added to bring IQueryable<> into scope.
+using System.Linq;
+// Added to bring XmlTextReader into scope.
+using System.Xml;
+// Added to bring WorkerRole into scope.
+using Microsoft.WindowsAzure.ServiceRuntime;
+
+namespace WorkerRoleUnitTests.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        FakeAzureDoctorDb db = new FakeAzureDoctorDb();
+
+        [TestInitialize]
+        public void FakeDataToFakeDb()
+        {
+            db.AddSet(TestData.Subscriptions);
+            db.AddSet(TestData.CloudServices);
+            db.AddSet(TestData.RequestResults);
+            db.AddSet(TestData.InstanceStatuses);
+            db.AddSet(TestData.Certificates);
+        }
+
+        [TestMethod]
+        public void ParseResults_Returns_AllInstancesHealthy_True()
+        {
+            // Arrange
+            XmlTextReader reader = new XmlTextReader("../../Fakes/FakeResultsHealthy.xml");
+
+            // Act
+            ParseResults pr = new ParseResults();
+            RequestResult rr = pr.ParseXmlText(reader);
+
+            // Assert
+            Assert.AreEqual(true, rr.AllInstancesHealthy);
+        }
+
+        [TestMethod]
+        public void ParseResults_Returns_AllInstancesHealthy_False()
+        {
+            // Arrange
+            XmlTextReader reader = new XmlTextReader("../../Fakes/FakeResultsUnhealthy.xml");
+
+            // Act
+            ParseResults pr = new ParseResults();
+            RequestResult rr = pr.ParseXmlText(reader);
+
+            // Assert
+            Assert.AreEqual(false, rr.AllInstancesHealthy);
+        }
+    }
+}
