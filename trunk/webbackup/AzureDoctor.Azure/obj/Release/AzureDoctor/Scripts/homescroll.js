bb284f9dfd3a5d61eb57bd7f0826323b3af15290
okeeman <okeeman@fe8eac2c-1429-41c5-6867-7c37e67d637c>
2013-05-17 20:49:07 +0000

    Remove web backup

--- trunk/webbackup/AzureDoctor.Azure/obj/Release/AzureDoctor/Scripts/homescroll.js
+++
@@ -1,6 +0,0 @@
-var textArray = 'Azure Doctor - The solution to your Windows Azure cloud service monitoring needs. ';
-
-function scrollText() {
-	textArray = textArray.substring(1, textArray.length) + textArray[0];
-	document.getElementById('homeScrollDiv').innerHTML = textArray;
-}
